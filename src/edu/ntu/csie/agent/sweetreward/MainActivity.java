18a9d16fc30a9dee34baa93cdcc7108d92faf46f
catlikethief <a50119729@gmail.com>
2013-05-30 16:14:26 +0800

    Added sending email as send feedback. (Should try bug report since it's more beautiful after putting on market)

--- src/edu/ntu/csie/agent/sweetreward/MainActivity.java
+++ src/edu/ntu/csie/agent/sweetreward/MainActivity.java
@@ -76,6 +76,25 @@ public class MainActivity extends Activity implements OnTaskCompleted {
 	    		intent = new Intent(this, ProblemListActivity.class);
 	            this.startActivity(intent);
 	    		break;
+	    		
+	    	case R.id.menu_send_feedback:
+	    		Intent Email = new Intent(Intent.ACTION_SEND);
+	            Email.setType("text/email");
+	            Email.putExtra(Intent.EXTRA_EMAIL, new String[] { "admin@hotmail.com" });
+	            Email.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+	            Email.putExtra(Intent.EXTRA_TEXT, "Dear SweetFeedback,\n\n");
+	            startActivity(Intent.createChooser(Email, "Send Feedback:"));
+	            break;
+	            
+	            // it seems this will work if the app is on the market
+	    		/*
+	    		intent = new Intent(Intent.ACTION_APP_ERROR);
+	    	    startActivity(intent);
+	    	    break;
+	    	    */
+	    		
+	    		
+	    		
     		default:
     			break;
     	}
