531a6369c096ede483486b32d1ab9339c860f580
catlikethief <a50119729@gmail.com>
2013-05-30 18:30:19 +0800

    Added check network status.

--- src/edu/ntu/csie/agent/sweetreward/ServerConnection.java
+++ src/edu/ntu/csie/agent/sweetreward/ServerConnection.java
@@ -17,6 +17,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Debug;
 import android.util.Log;
@@ -25,14 +27,17 @@ import android.widget.Toast;
 
 public class ServerConnection {
 	private static final String TAG = ServerConnection.class.getSimpleName();
+	
+	public static ServerConnection sSingleton;
 
 	private String APIPath = "/";
 	private String APIDomain = "http://disa.csie.ntu.edu.tw:1234";
 	
 	private User mUser = User.getUser();
 	
-	public static ServerConnection sSingleton;
 	
+	private static ConnectivityManager mConnectivityManager;
+	private Context mAppContext;
 	
 	public static ServerConnection getServerConnection() {
 		if(sSingleton == null)
@@ -40,8 +45,30 @@ public class ServerConnection {
 	    return sSingleton;
 	}
 	
+	public void setContext(Context context) {
+		mAppContext = context;
+	}
+	
+	private Boolean isNetworkAvailable() {
+		if(null == mConnectivityManager) {  
+            mConnectivityManager = (ConnectivityManager)mAppContext.getSystemService(Context.CONNECTIVITY_SERVICE);  
+        }
+        NetworkInfo wifiInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (wifiInfo.isAvailable()) {
+            return true;
+        } else if (mobileInfo.isAvailable()) {
+            return true;
+        } else {
+            return false;
+        }
+	}
+	
 	public void login(String account, String password, OnTaskCompleted listener) {
-		
+		if (!isNetworkAvailable()) {
+			Log.d(TAG, "login, NO network");
+			return;
+		}
     	String httpUrl = String.format("%s/%s/mobile/createNewUser.php?account=%s&password=%s", APIDomain, APIPath, account, password);
     	
     	ServerTask task = new ServerTask(listener);
@@ -49,6 +76,11 @@ public class ServerConnection {
     }
 	
 	public void reportWindow(OnTaskCompleted listener, int windowID, int action) {
+		if (!isNetworkAvailable()) {
+			Log.d(TAG, "report window, NO network");
+			return;
+		}
+		
 	    String token = mUser.getToken();
 		String httpUrl = "";
 		if (action == -1) {
@@ -62,6 +94,11 @@ public class ServerConnection {
 	}
 	
 	public void getProblemList(OnTaskCompleted listener) {
+		if (!isNetworkAvailable()) {
+			Log.d(TAG, "get problem list, NO network");
+			return;
+		}
+		
 		String api = "reports/unsolved";
 		String httpUrl = String.format("%s/%s", APIDomain, api);
 		ServerTask task = new ServerTask(listener);
