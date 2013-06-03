0c04646f620752cda429b67c638535490109fe49
catlikethief <a50119729@gmail.com>
2013-06-04 00:28:00 +0800

    Added seek bar in notification alert dialog.

--- src/edu/ntu/csie/agent/sweetreward/TaskActivity.java
+++ src/edu/ntu/csie/agent/sweetreward/TaskActivity.java
@@ -5,36 +5,49 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class TaskActivity extends Activity {
 	private static final String TAG = TaskActivity.class.getSimpleName();
+	
+	SeekBar mSeekBar;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		// 1. Instantiate an AlertDialog.Builder with its constructor
-    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    	Bundle bundle = this.getIntent().getExtras();
-    	
-    	String msg = bundle.getString("content");
+		Bundle bundle = this.getIntent().getExtras();
+		String msg = bundle.getString("content");
     	Log.d(TAG, "content: " + msg);
-    	builder.setMessage(msg).setTitle("Would you please ...");
+		
+    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    	LayoutInflater inflater = getLayoutInflater();;
+    	View view = inflater.inflate(R.layout.dialog_notification, null);
+    	mSeekBar = (SeekBar) view.findViewById(R.id.dialog_notification_annoy_seekbar);
+    	//TextView msgTextView = (TextView) view.findViewById(R.id.dialog_notification_msg_textview);
+    	//msgTextView.setText(msg);
+    	
+    	builder.setView(view);
+    	builder.setMessage(msg + "\n\nDo you feel annoyed?");
+    	builder.setTitle("Would you please ...");
     	builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
     	           public void onClick(DialogInterface dialog, int id) {
-    	        	   Log.d(TAG, getString(R.string.ok));
+    	        	   Log.d(TAG, "user annoyied: " + String.valueOf(mSeekBar.getProgress()));
     	        	   finish();
     	           }
     	       });
     	builder.setNegativeButton(getString(R.string.cencel), new DialogInterface.OnClickListener() {
     	           public void onClick(DialogInterface dialog, int id) {
-    	        	   Log.d(TAG, getString(R.string.cencel));
+    	        	   Log.d(TAG, "user annoyied: " + String.valueOf(mSeekBar.getProgress()));
     	               finish();
     	           }
     	       });
-
-    	// 3. Get the AlertDialog from create()
     	AlertDialog dialog = builder.create();
+    	
     	dialog.show();
 	}
 
