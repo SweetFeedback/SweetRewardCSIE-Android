42504e4f314baf3d321beabf53e5733c5282bd9e
catlikethief <a50119729@gmail.com>
2013-06-09 16:10:09 +0800

    When answering the notification, call API to insert record to database.

--- src/edu/ntu/csie/agent/sweetreward/ServerConnection.java
+++ src/edu/ntu/csie/agent/sweetreward/ServerConnection.java
@@ -28,75 +28,91 @@ import android.widget.Toast;
 
 public class ServerConnection {
 	private static final String TAG = ServerConnection.class.getSimpleName();
-	
+
 	public static ServerConnection sSingleton;
 
 	private String APIPath = "/";
 	private String APIDomain = "http://disa.csie.ntu.edu.tw:1234";
-	
+
 	private User mUser = User.getUser();
-	
-	
+
+
 	private static ConnectivityManager mConnectivityManager;
 	private Context mAppContext;
-	
+
 	public static ServerConnection getServerConnection() {
 		if(sSingleton == null)
 			sSingleton = new ServerConnection();
-	    return sSingleton;
+		return sSingleton;
 	}
-	
+
 	public void setContext(Context context) {
 		mAppContext = context;
 	}
-	
+
 	private Boolean isNetworkAvailable() {
 		if(null == mConnectivityManager) {  
-            mConnectivityManager = (ConnectivityManager)mAppContext.getSystemService(Context.CONNECTIVITY_SERVICE);  
-        }
-        NetworkInfo wifiInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        NetworkInfo mobileInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        if (wifiInfo.isAvailable()) {
-            return true;
-        } else if (mobileInfo.isAvailable()) {
-            return true;
-        } else {
-            return false;
-        }
+			mConnectivityManager = (ConnectivityManager)mAppContext.getSystemService(Context.CONNECTIVITY_SERVICE);  
+		}
+		NetworkInfo wifiInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		NetworkInfo mobileInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		if (wifiInfo.isAvailable()) {
+			return true;
+		} else if (mobileInfo.isAvailable()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+
+	public Boolean responseNotification(int ok, int annoy_level) {
+		if (!isNetworkAvailable()) {
+			Log.d(TAG, "login, NO network");
+			return false;
+		}
+		
+		String user_id = "223";
+		String task_id = "11";
+		String httpUrl = String.format("%s/notification_response?task_id=%s&user_id=%s&ok=%d&annoy_level=%d", APIDomain, task_id, user_id, ok, annoy_level);
+		Log.d(TAG, httpUrl);
+		ServerTask task = new ServerTask();
+		task.execute(httpUrl);
+
+		return true;
+	}
+
 	public Boolean login(String account, String password, OnTaskCompleted listener) {
 		if (!isNetworkAvailable()) {
 			Log.d(TAG, "login, NO network");
 			return false;
 		}
-    	String httpUrl = String.format("%s/%s/mobile/createNewUser.php?account=%s&password=%s", APIDomain, APIPath, account, password);
-    	
-    	ServerTask task = new ServerTask(listener);
-    	task.execute(httpUrl);
-    	return true;
-    }
-	
+		String httpUrl = String.format("%s/%s/mobile/createNewUser.php?account=%s&password=%s", APIDomain, APIPath, account, password);
+
+		ServerTask task = new ServerTask(listener);
+		task.execute(httpUrl);
+		return true;
+	}
+
 	public Boolean reportWindow(OnTaskCompleted listener, int windowID, int action) {
 		if (!isNetworkAvailable()) {
 			Log.d(TAG, "report window, NO network");
 			return false;
 		}
-		
-	    String token = mUser.getToken();
+
+		String token = mUser.getToken();
 		String httpUrl = "";
 		if (action == -1) {
 			httpUrl = String.format("%s/%s/userActionTrigger.php?window_id=%d&token=%s", APIDomain, APIPath, windowID, token);
 		} else if (action != -1) {
 			httpUrl= String.format("%s/%s/userActionTrigger.php?window_id=%d&token=%s&action=%d", APIDomain, APIPath, windowID, token, action);
 		}
-		
+
 		ServerTask task = new ServerTask(listener);
 		task.execute(httpUrl);
-		
+
 		return true;
 	}
-	
+
 	public Boolean reportProblem(OnTaskCompleted listener) {
 		if (!isNetworkAvailable()) {
 			Log.d(TAG, "login, NO network");
@@ -108,7 +124,7 @@ public class ServerConnection {
 		//task.execute(httpUrl);
 		return true;
 	}
-	
+
 
 	public Boolean getProblemList(OnTaskCompleted listener) {
 		if (!isNetworkAvailable()) {
@@ -120,52 +136,58 @@ public class ServerConnection {
 		String httpUrl = String.format("%s/%s", APIDomain, api);
 		ServerTask task = new ServerTask(listener);
 		task.execute(httpUrl);
-		
+
 		return true;
 	}
-    
-    private class ServerTask extends AsyncTask <String, Integer, String> {
-        private OnTaskCompleted listener;
-        
-        public ServerTask(OnTaskCompleted listener) {
-            this.listener = listener;
-        }
-        
-        @Override
-        protected void onPreExecute() {
-        }
-        
-    	@Override
+
+	private class ServerTask extends AsyncTask <String, Integer, String> {
+		private OnTaskCompleted listener;
+
+		public ServerTask() {
+			this.listener = null;
+		}
+
+		public ServerTask(OnTaskCompleted listener) {
+			this.listener = listener;
+		}
+
+		@Override
+		protected void onPreExecute() {
+		}
+
+		@Override
 		protected String doInBackground(String... params) {
-            HttpGet request = new HttpGet(params[0]);
-            HttpClient httpClient = new DefaultHttpClient();
-            
-            Log.d(TAG, "Run request: " + request);
-            
-            try {
-                HttpResponse response = httpClient.execute(request);
-                if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                	String str = EntityUtils.toString(response.getEntity());
-                	return str;
-                }
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();    
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-    	
+			HttpGet request = new HttpGet(params[0]);
+			HttpClient httpClient = new DefaultHttpClient();
+
+			Log.d(TAG, "Run request: " + request);
+
+			try {
+				HttpResponse response = httpClient.execute(request);
+				if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+					String str = EntityUtils.toString(response.getEntity());
+					return str;
+				}
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();    
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 			return null;
 		}
-		
-
-	    @Override
-	    protected void onPostExecute(String result) {
-	        super.onPostExecute(result);
-	        listener.onTaskCompleted(result);
-	    }
-    }
-    
 
 
-	
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			if (listener != null) {
+				listener.onTaskCompleted(result);
+			}
+		}
+	}
+
+
+
+
 }
--- src/edu/ntu/csie/agent/sweetreward/TaskActivity.java
+++ src/edu/ntu/csie/agent/sweetreward/TaskActivity.java
@@ -15,45 +15,48 @@ import android.widget.TextView;
 public class TaskActivity extends Activity {
 	private static final String TAG = TaskActivity.class.getSimpleName();
 	
-	SeekBar mSeekBar;
-	
+	private SeekBar mSeekBar;
+	private ServerConnection mServerConnection;
+
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		Bundle bundle = this.getIntent().getExtras();
 		String msg = bundle.getString("content");
-    	Log.d(TAG, "content: " + msg);
-    	
-    	
-		
-    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    	LayoutInflater inflater = getLayoutInflater();;
-    	View view = inflater.inflate(R.layout.dialog_notification, null);
-    	mSeekBar = (SeekBar) view.findViewById(R.id.dialog_notification_annoy_seekbar);
+		Log.d(TAG, "content: " + msg);
 
-    
-    	//TextView msgTextView = (TextView) view.findViewById(R.id.dialog_notification_msg_textview);
-    	//msgTextView.setText(msg);
-    	
-    	builder.setView(view);
-    	builder.setMessage(msg + "\n\nDo you feel annoyed?");
-    	builder.setTitle("Would you please ...");
-    	builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
-    	           public void onClick(DialogInterface dialog, int id) {
-    	        	   Log.d(TAG, "user annoyied: " + String.valueOf(mSeekBar.getProgress()));
-    	        	   finish();
-    	           }
-    	       });
-    	builder.setNegativeButton(getString(R.string.cencel), new DialogInterface.OnClickListener() {
-    	           public void onClick(DialogInterface dialog, int id) {
-    	        	   Log.d(TAG, "user annoyied: " + String.valueOf(mSeekBar.getProgress()));
-    	               finish();
-    	           }
-    	       });
-    	AlertDialog dialog = builder.create();
-    	
-    	dialog.show();
+		mServerConnection = ServerConnection.getServerConnection();
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		LayoutInflater inflater = getLayoutInflater();;
+		View view = inflater.inflate(R.layout.dialog_notification, null);
+		mSeekBar = (SeekBar) view.findViewById(R.id.dialog_notification_annoy_seekbar);
+
+
+		//TextView msgTextView = (TextView) view.findViewById(R.id.dialog_notification_msg_textview);
+		//msgTextView.setText(msg);
+
+		builder.setView(view);
+		builder.setMessage(msg + "\n\nDo you feel annoyed?");
+		builder.setTitle("Would you please ...");
+		builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				int annoy = mSeekBar.getProgress();
+				mServerConnection.responseNotification(1, annoy);
+				finish();
+			}
+		});
+		builder.setNegativeButton(getString(R.string.cencel), new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				int annoy = mSeekBar.getProgress();
+				mServerConnection.responseNotification(0, annoy);
+				finish();
+			}
+		});
+		AlertDialog dialog = builder.create();
+
+		dialog.show();
 	}
 
 }
