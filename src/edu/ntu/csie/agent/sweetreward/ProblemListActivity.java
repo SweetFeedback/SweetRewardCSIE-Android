a328ef1fab44092d8cae265b425e27b8ec83f1ef
catlikethief <a50119729@gmail.com>
2013-06-11 20:33:42 +0800

    Refactored some code.

--- src/edu/ntu/csie/agent/sweetreward/ProblemListActivity.java
+++ src/edu/ntu/csie/agent/sweetreward/ProblemListActivity.java
@@ -10,9 +10,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -27,25 +24,27 @@ import android.widget.TextView;
 
 public class ProblemListActivity extends Activity implements OnTaskCompleted {
 	private static final String TAG = ProblemListActivity.class.getSimpleName();
-	
+
 	private ListView mProblemList;
 	private LinearLayout mProgress;
 	private TextView mNoNetworkTextView;
 	private Handler mUpdateHandler;
 	
+	private int mUpdateInterival = 5000;
+
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		Log.d(TAG, "Start ProblemListActivity");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_problem_list);
-        
-        mUpdateHandler = new Handler();
-        
-        mProgress = (LinearLayout) findViewById(R.id.headerProgressLinearLayout);
-        mProblemList = (ListView) findViewById(R.id.ProblemListLinearLayout);
-        
-        mNoNetworkTextView = (TextView) findViewById(R.id.noNetworkTextView_ProblemList);
-        mNoNetworkTextView.setOnClickListener(new OnClickListener() {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_problem_list);
+
+		mUpdateHandler = new Handler();
+
+		mProgress = (LinearLayout) findViewById(R.id.headerProgressLinearLayout);
+		mProblemList = (ListView) findViewById(R.id.ProblemListLinearLayout);
+
+		mNoNetworkTextView = (TextView) findViewById(R.id.noNetworkTextView_ProblemList);
+		mNoNetworkTextView.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -53,45 +52,44 @@ public class ProblemListActivity extends Activity implements OnTaskCompleted {
 				startActivity(intent);
 			}
 		});
-    }
-	
-	private int mUpdateInterival = 5000;
-	
-    @Override
-    protected void onResume() {
-    	super.onResume();
-    	
-    	mProgress.setVisibility(View.VISIBLE);
-    	Boolean success = ServerConnection.getServerConnection().getProblemList(ProblemListActivity.this);
-    	if (!success) {
-    		mNoNetworkTextView.setVisibility(View.VISIBLE);
-    		mUpdateHandler.postDelayed(mUpdateTaskRunnable, mUpdateInterival);
-    	}
-    }
-    
-    
+	}
 
-    private Runnable mUpdateTaskRunnable = new Runnable() {
-	    public void run() {
-	    	Log.d(TAG, "RUN");
-	    	Boolean success = ServerConnection.getServerConnection().getProblemList(ProblemListActivity.this);
-	    	if (!success) {
-	    		mUpdateHandler.postDelayed(this, mUpdateInterival);
-	        } else {
-	        	mNoNetworkTextView.setVisibility(View.GONE);
-	        	mUpdateHandler.removeCallbacks(mUpdateTaskRunnable);
-	        }
-	    }
-    };
-    
-    
 	
+
 	@Override
-    public void onTaskCompleted(String jsonString) {
+	protected void onResume() {
+		super.onResume();
+
+		mProgress.setVisibility(View.VISIBLE);
+		Boolean success = ServerConnection.getServerConnection().getProblemList(ProblemListActivity.this);
+		if (!success) {
+			mNoNetworkTextView.setVisibility(View.VISIBLE);
+			mUpdateHandler.postDelayed(mUpdateTaskRunnable, mUpdateInterival);
+		}
+	}
+
+
+
+	private Runnable mUpdateTaskRunnable = new Runnable() {
+		public void run() {
+			Boolean success = ServerConnection.getServerConnection().getProblemList(ProblemListActivity.this);
+			if (!success) {
+				mUpdateHandler.postDelayed(this, mUpdateInterival);
+			} else {
+				mNoNetworkTextView.setVisibility(View.GONE);
+				mUpdateHandler.removeCallbacks(mUpdateTaskRunnable);
+			}
+		}
+	};
+
+
+
+	@Override
+	public void onTaskCompleted(String jsonString) {
 		Log.d(TAG, "Received server result");
 		List<Map<String,String>> problemList = new ArrayList<Map<String,String>>();
-    	// parse result
-    	JSONObject json = null;
+		// parse result
+		JSONObject json = null;
 		try {
 			if (jsonString == null) {
 				Log.d(TAG, "Return jsonString null");
@@ -104,7 +102,7 @@ public class ProblemListActivity extends Activity implements OnTaskCompleted {
 				JSONObject data = dataArray.getJSONObject(i);
 				dataMap.put("title", data.getString("title"));
 				dataMap.put("created_at", data.getString("created_at"));
-				
+
 				problemList.add(dataMap);
 			}
 		} catch (JSONException e) {
@@ -113,8 +111,8 @@ public class ProblemListActivity extends Activity implements OnTaskCompleted {
 
 		// set adapter
 		Log.d(TAG, "Set listview adapter");
-        SimpleAdapter adapter = new SimpleAdapter(this, problemList, android.R.layout.simple_list_item_2, new String[] {"title", "created_at"}, new int[] { android.R.id.text1, android.R.id.text2 });
-        mProblemList.setAdapter(adapter);
-        this.mProgress.setVisibility(View.GONE);    
+		SimpleAdapter adapter = new SimpleAdapter(this, problemList, android.R.layout.simple_list_item_2, new String[] {"title", "created_at"}, new int[] { android.R.id.text1, android.R.id.text2 });
+		mProblemList.setAdapter(adapter);
+		this.mProgress.setVisibility(View.GONE);    
 	}
 }
